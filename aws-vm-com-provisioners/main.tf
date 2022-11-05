1131e73e41b46c27c003374227d83219d082c618
Roberto Duarte <robertobduarte@gmail.com>
2022-11-05 10:32:04 -0300

    aws-vm-com-provisioners

---
+++ aws-vm-com-provisioners/main.tf
@@ -0,0 +1,29 @@
+terraform {
+  required_version = ">= 1.0.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "3.73.0"
+    }
+  }
+
+  backend "s3" {
+    bucket = "bucket-outputs-terraform-roberto-duarte"
+    key    = "aws-vm-provisioners/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+data "terraform_remote_state" "vpc" {
+  backend = "s3"
+  config = {
+    bucket = "bucket-outputs-terraform-roberto-duarte"
+    key    = "aws-vpc/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
