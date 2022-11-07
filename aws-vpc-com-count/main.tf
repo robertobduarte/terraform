3fdb01a83cd32132689b9c8a2df7a83d35ea5ef1
Roberto Duarte <robertobduarte@gmail.com>
2022-11-07 15:38:52 -0300

    aws-vpc-com-count

---
+++ aws-vpc-com-count/main.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_version = ">= 1.0.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "3.73.0"
+    }
+  }
+
+  backend "s3" {
+    bucket = "bucket-outputs-terraform-roberto-duarte"
+    key    = "aws-vpc-com-count/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
+provider "aws" {
+  region = var.region
+}
---
+++ aws-vpc-com-count/variable.tf
@@ -0,0 +1,5 @@
+variable "region" {
+  type        = string
+  default     = "us-east-1"
+  description = "Região da AWS escolhida"
+}
